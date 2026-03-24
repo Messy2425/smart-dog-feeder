508da6ed7853edb7a9065751a33c131ad5246ab9
Meslin <meslinmeslin8@gmail.com>
2026-03-24 21:43:02 +0530

    Final fix for MongoDB connection and deployment settings

--- backend/server.js
+++ backend/server.js
@@ -21,8 +21,7 @@ const connectDB = async () => {
   
   try {
     const db = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000
     });
     isConnected = db.connections[0].readyState;
     console.log('Connected to MongoDB');
